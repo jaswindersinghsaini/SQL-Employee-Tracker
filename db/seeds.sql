f1e5bce4b508a1ca6ed841f73d2731d47d693870
Jaswinder Singh <jaswindersinghsaini@gmail.com>
2023-06-10 18:18:38 -0700

    created schema and seeds sql files with data

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO department (name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+       ("Sales"); 
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 4),
+       ("Salesperson", 80000, 4),
+       ("Lead Engineer", 150000, 1),
+       ("Software Engineer", 120000, 1),
+       ("Account Manager", 160000, 2),
+       ("Accountant", 125000, 2),
+       ("Legal Team Lead", 250000, 3),
+       ("Lawyer", 190000, 3);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Khoa", "Nguyen", 4, 2),
+       ("Rolyn", "Palma", 4, 4),
+       ("Sixto", "Alvarado", 1, 3),
+       ("Kalli", "Rai", 1, 1),
+       ("Jose", "Alvarado", 2, 3),
+       ("Carlo", "DelaCruz", 2, 2),
+       ("Tom", "Coonce", 3, NULL),
+       ("Joel", "MC", 3, 2); 
